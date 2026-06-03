2f908a56f2f27c043cfba4d7e68c8c3855555528
alkharivotito <alkharivoito19@gmail.com>
2026-06-03 10:58:10 +0700

    tambah kode

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
-import mysql2 from "mysql2"
+import mysql2 from "mysql2";
+
 const db = new Sequelize('bukumahapro', 'root', '', {
       host: "localhost",
       dialect: "mysql",
